f3be4ef728399f5f2275ec9c198c30be905005b3
John Beatrice <johncbeatrice@gmail.com>
2025-04-15 17:27:44 +0000

    notes for salon.sh

--- salon.sh
+++ salon.sh
@@ -5,32 +5,18 @@ PSQL="psql --username=freecodecamp --dbname=salon -t --no-align -c"
 echo -e "\n~~ Salon Appointment Scheduler ~~\n"
 
 
-# MAIN_MENU function opens when salon.sh is run
-# "Enter the number of the service you would like to book"
-# display services name and service_ids
-# wait for user input, 
-# check if user input is valid (case statement)
-# else display error message and services options again
-# maybe use a loop for this?
-# while valid_answer=false
-#   case/if statement with conditions
-#   when valid answer set valid_answer to true
-# use a loop for every customer input you need, keep looping until
-# they give you valid inputs
 
-# once you have all the customer/appointment info,
-# check if customer exists in the customer table, if not, add them
-# then create row in appointments table
+main_menu function will display welcome message and appointment options
 
-# to check if customer already exists, check their phone number
+get_user_input function will contain the loops to get all user input
+it will query the db to check whether the customer is new
+if customer is new, add customer to customers table
+then add info as new row to appointments table
+then display the successful appointment booking message
+then exit the script
+(it will be called within main_menu after main_menu displays the services options)
 
-
-# after user inputs service_id
-# run get_customer_info function 
-
-
-
-
-
-
+exit function will be called within main_menu in the case that the user
+enters the exit code instead of a valid service_id
+it will display a goodbye/exit message and then terminate the script
 
