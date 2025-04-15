a009aeb509c91196c43d3149b22653af4c78b97a
John Beatrice <johncbeatrice@gmail.com>
2025-04-15 18:28:21 +0000

    main_menu function

--- salon.sh
+++ salon.sh
@@ -1,22 +1,35 @@
 #! /bin/bash
 
+# salon appointment scheduler script
+
+# connect to salon database
 PSQL="psql --username=freecodecamp --dbname=salon -t --no-align -c"
 
+# function that displays main menu
+main_menu () {
+  echo -e "Please type the number of the service\n you would like to schedule:\n"
+  echo "$($PSQL "SELECT * FROM services;")" | sed 's/|/) /'
+  echo -e "4) Exit\n"
+}
+
+get_user_input () {
+  echo "hi"
+}
+
 echo -e "\n~~ Salon Appointment Scheduler ~~\n"
+main_menu
 
+# main_menu function will display welcome message and appointment options
 
+# get_user_input function will contain the loops to get all user input
+# it will query the db to check whether the customer is new
+# if customer is new, add customer to customers table
+# then add info as new row to appointments table
+# then display the successful appointment booking message
+# then exit the script
+# (it will be called within main_menu after main_menu displays the services options)
 
-main_menu function will display welcome message and appointment options
-
-get_user_input function will contain the loops to get all user input
-it will query the db to check whether the customer is new
-if customer is new, add customer to customers table
-then add info as new row to appointments table
-then display the successful appointment booking message
-then exit the script
-(it will be called within main_menu after main_menu displays the services options)
-
-exit function will be called within main_menu in the case that the user
-enters the exit code instead of a valid service_id
-it will display a goodbye/exit message and then terminate the script
+# exit function will be called within main_menu in the case that the user
+# enters the exit code instead of a valid service_id
+# it will display a goodbye/exit message and then terminate the script
 
