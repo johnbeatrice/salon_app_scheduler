bf2a437f5790169a964a99c754a9e88e79968a64
John Beatrice <johncbeatrice@gmail.com>
2025-04-21 02:51:02 +0000

    mimicing example 1

--- salon.sh
+++ salon.sh
@@ -13,9 +13,9 @@ do_not_show_new_customer_insert_statement=''
 
 # function that displays main menu
 main_menu () {
-  echo -e "\nPlease type the number of the service\n you would like to schedule:\n"
+  # echo -e "\nWelcome to My Salon, how can I help you?\n"
   echo "$($PSQL "SELECT * FROM services;")" | sed 's/|/) /'
-  echo -e "4) Exit\n"
+  echo -e "4) Exit"
 }
 
 get_user_input () {
@@ -27,35 +27,35 @@ do
 read SERVICE_ID_SELECTED
   if [[ $SERVICE_ID_SELECTED == 1 ]];
     then
-      echo "You have selected Haircut"
+      # echo "You have selected Haircut"
       break
   elif [[ $SERVICE_ID_SELECTED == 2 ]];
     then
-      echo "You have selected Manicure"
+      # echo "You have selected Manicure"
       break
   elif [[ $SERVICE_ID_SELECTED == 3 ]];
     then
-      echo "You have selected Pedicure"
+      # echo "You have selected Pedicure"
       break
   elif [[ $SERVICE_ID_SELECTED == 4 ]];
     then
       echo -e "Good-bye"
       exit
   else 
-    # echo "Please enter a valid input"
+    echo -e "\nI could not find that service. What would you like today?"
     main_menu
   fi
 done
 
 # echo -e "loop1 is done\n"
 
-echo "Please enter your phone number in the format of 555-555-5555"
+echo -e "\nWhat's your phone number?"
 while [ cont_loop=true ];
 do
 read CUSTOMER_PHONE
   if [[ $CUSTOMER_PHONE =~ $valid_phone_num ]];
     then
-    echo $CUSTOMER_PHONE
+    # echo $CUSTOMER_PHONE
       break
   else 
     echo "Please enter a valid phone number:"
@@ -71,7 +71,7 @@ cust_exists="$($PSQL "SELECT * FROM customers WHERE phone = '$CUSTOMER_PHONE';")
 if [[ -z $cust_exists ]];
 then
 
-echo "Please enter your name:"
+echo -e "\nI don't have a record for that phone number, what's your name?"
 
 while [ cont_loop=true ];
 do
@@ -86,10 +86,10 @@ read CUSTOMER_NAME
 done
   # add new customer to salon database
  echo "$($PSQL "INSERT INTO customers (name, phone) VALUES ('$CUSTOMER_NAME', '$CUSTOMER_PHONE');")" | $do_not_show_new_customer_insert_statement
- echo -e "Welcome new customer!\n"
+#  echo -e "Welcome new customer!\n"
 fi
 
-echo "Please enter the preferred time for your appointment:"
+echo -e "\nWhat time would you like your cut, $CUSTOMER_NAME?"
 while [ cont_loop=true ];
 do
 read SERVICE_TIME
@@ -111,7 +111,7 @@ if [[ -z $cust_exists ]];
 then
   # add new customer to salon database
  echo "$($PSQL "INSERT INTO customers (name, phone) VALUES ('$CUSTOMER_NAME', '$CUSTOMER_PHONE');")" | $do_not_show_new_customer_insert_statement
- echo -e "Welcome new customer!\n"
+#  echo -e "Welcome new customer!\n"
 fi
 
 }
@@ -124,10 +124,11 @@ create_appointment () {
   echo "$($PSQL "INSERT INTO appointments (customer_id, service_id, time) VALUES ('$customer_id', '$SERVICE_ID_SELECTED', '$SERVICE_TIME');")" | $do_not_show_new_customer_insert_statement
 
   # appointment confirmation
-  echo -e "I have put you down for a $service_name at $SERVICE_TIME, $CUSTOMER_NAME."
+  echo -e "\nI have put you down for a $service_name at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
-echo -e "\n~~ Salon Appointment Scheduler ~~\n"
+echo -e "\n~~~~ MY SALON ~~~~\n"
+echo -e "\nWelcome to My Salon, how can I help you?\n"
 main_menu
 get_user_input
 create_appointment
